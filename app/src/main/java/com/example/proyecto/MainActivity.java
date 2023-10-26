583a6e29c116c43ef0a602b4eab503a8008158fc
AkagamiDZ <matiasgold18@gmail.com>
2023-10-26 11:28:13 -0300

    ver.1

---
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button BTInicio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this,MainActivity2.class);
+        BTInicio = (Button) findViewById(R.id.BTInicio);
+        BTInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){startActivity(intent); }
+        });
+    }
+}
